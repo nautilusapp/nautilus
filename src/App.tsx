b0161513f93a1af4225bce88f8cecb88593f567c
Joshua Nordstrom <jdnordy@gmail.com>
2020-03-11 13:54:25 -0700

    added LeftNav container

--- src/App.tsx
+++ src/App.tsx
@@ -8,11 +8,10 @@
  *
  * ************************************
  */
-
 import React, { Component } from 'react';
-import { render } from 'react-dom';
 
 // IMPORT REACT CONTAINERS OR COMPONENTS
+import { LeftNav } from './containers/LeftNav';
 import Title from './components/Title';
 import Button from 'react-bootstrap/Button';
 import FileSelector from './components/FileUpload';
@@ -20,21 +19,27 @@ import FileSelector from './components/FileUpload';
 //bootstrap css
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-type AppState = {
+type State = {
   projectName: string;
 };
 
-class App extends Component<{}, AppState> {
-  state = {
-    projectName: 'Nautilus',
-  };
+
+class App extends Component<{}, State> {
+  constructor(props: {}) {
+    super(props);
+    this.state = {
+      projectName: 'Nautilus',
+    }
+
+  }
   render() {
     return (
-      <>
+      <div>
         <Title projectName={this.state.projectName} />
+        <LeftNav />
         <Button variant="secondary">hello noobs</Button>
         <FileSelector />
-      </>
+      </div>
     );
   }
 }
---
+++ src/containers/LeftNav.tsx
@@ -0,0 +1,24 @@
+/**
+ * ************************************
+ *
+ * @module  LeftNav.tsx
+ * @author 
+ * @date 3/11/20
+ * @description 
+ * 
+ * ************************************
+ */
+import React from 'react';
+
+type Props = {
+
+}
+
+export const LeftNav: React.FC<Props> = props => {
+
+  return (
+    <div>
+      
+    </div>
+  );
+}
