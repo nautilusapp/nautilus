c6bc71fa941838557f4a8b0a662992dbb82e5ea9
Joshua Nordstrom <jdnordy@gmail.com>
2020-03-11 20:11:19 -0700

    types for state

--- src/App.tsx
+++ src/App.tsx
@@ -18,24 +18,49 @@ import './styles/app.scss';
 import LeftNav from './components/LeftNav';
 import OptionBar from './components/OptionBar';
 import D3Wrapper from './components/D3Wrapper';
+import Button from 'react-bootstrap/Button';
 
-type State = {
-  projectName: string;
+import { State } from './App.d';
+
+const initialState: State = {
+  selectedContainer: '',
+  fileUploaded: false,
+  services: {},
+  dependsOn: {},
+  networks: {},
+  volumes: [],
+  volumesClicked: {},
+  bindMounts: [],
+  bindMountsClicked: {},
+  view: 'default',
+  options: {
+    ports: false,
+    volumes: false,
+    dependsOn: false,
+  },
 };
-
 
 class App extends Component<{}, State> {
   constructor(props: {}) {
     super(props);
-    this.state = {
-      projectName: 'Nautilus',
-    }
+    this.state = initialState;
 
+    this.fileUploaded = this.fileUploaded.bind(this);
   }
+
+  fileUploaded() {
+    this.setState(state => {
+      return {
+        ...state,
+        fileUploaded: state.fileUploaded ? false : true,
+      };
+    });
+  }
+
   render() {
     return (
-      <div className="app" >
-        <LeftNav projectName={this.state.projectName} />
+      <div className="app">
+        <LeftNav fileUploaded={this.fileUploaded} />
         <OptionBar />
         <D3Wrapper />
       </div>
