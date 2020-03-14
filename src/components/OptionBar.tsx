c98b1e6216571e7eb5092c07b53392cfe4eb2104
Sweet T <thurtt@rocketmail.com>
2020-03-13 18:35:29 -0700

    Added option bar and styling, as well as formatted the app to be in grid layout, changed small aspects of left nav

--- src/components/OptionBar.tsx
+++ src/components/OptionBar.tsx
@@ -9,14 +9,33 @@
  * ************************************
  */
 import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Form from 'react-bootstrap/Form';
+import FormControl from 'react-bootstrap/FormControl';
+import Button from 'react-bootstrap/Button';
 
 type Props = {};
 
 const OptionBar: React.FC<Props> = props => {
+  // ports: false,
+  // volumes: false,
+  // dependsOn: false,
+
   return (
-    <div className="option-bar">
-      <div></div>
-    </div>
+    <Navbar className="option-bar" variant="dark">
+      <Nav className="group-by">
+        <Navbar.Text>Group By: </Navbar.Text>
+        <Nav.Link className="option">Networks</Nav.Link>
+        <Nav.Link className="option">Depends On</Nav.Link>
+      </Nav>
+      <Nav className="view-options">
+        <Navbar.Text>View Options: </Navbar.Text>
+        <Nav.Link className="option" >Ports</Nav.Link>
+        <Nav.Link className="option" >Memory</Nav.Link>
+        <Nav.Link className="option" >Depends On</Nav.Link>
+      </Nav>
+    </Navbar>
   );
 };
 
