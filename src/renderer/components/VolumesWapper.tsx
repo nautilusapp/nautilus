dbbc117773bd88d2686a51563e96a1680d83719c
Joshua Nordstrom <jdnordy@gmail.com>
2020-03-17 17:37:38 -0700

    added a volume wrapper

---
+++ src/renderer/components/VolumesWapper.tsx
@@ -0,0 +1,24 @@
+/**
+ * ************************************
+ *
+ * @module  VolumesWrapper.tsx
+ * @author
+ * @date 3/17/20
+ * @description Display area for the volumes and bindmounts
+ * ************************************
+ */
+import React from 'react';
+// import HostOS from './HostOS';
+// import DockerEngine from './DockerEngine';
+
+type Props = {};
+
+const VolumesWrapper: React.FC<Props> = props => {
+  return (
+    <div className="volumes-wrapper">
+      <div></div>
+    </div>
+  );
+};
+
+export default VolumesWrapper;
