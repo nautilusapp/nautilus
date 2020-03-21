b92e81d7b9f4fd77a96dfdd65cc13ffe351ac30b
Danny Scheiner <dscheiner27@gmail.com>
2020-03-21 12:19:12 -0700

    added functionality to handle build options in the infoDropdown

--- src/renderer/components/InfoDropdown.tsx
+++ src/renderer/components/InfoDropdown.tsx
@@ -62,20 +62,15 @@ const InfoDropdown: React.FC<ReactProps> = ({ service, selectedContainer }) => {
   const environmentVariables: TwoDimension = {};
   const env_file: string[] = [];
 
-  // if service exists
   if (service) {
-    // console.log(YAML.stringify(service, 4));
-    // loop through the Commands in each service
     Object.keys(service).forEach(command => {
-      // and if the service command can be found in the list of docker-compose Commands...
       if (dockerComposeCommands[command]) {
-        // ...then set the command in the serviceOverview's object to equal the service property and its value to be the service property value
-        // ie {Build: ./result}
         serviceOverview[command] = '';
-        // ASIDE: if any of the below keys equal the specified strings,
+        //  *********************
+        //  * Environment
+        //  *********************
         if (command === 'environment') {
           if (Array.isArray(service[command])) {
-            // loop through the environment variable's values (array)
             service[command].forEach((value: string) => {
               const valueArray = value.split('=');
               environmentVariables[valueArray[0]] = valueArray[1];
@@ -86,6 +81,9 @@ const InfoDropdown: React.FC<ReactProps> = ({ service, selectedContainer }) => {
               environmentVariables[key] = environment[key];
             });
           }
+          //  *********************
+          //  Env_File
+          //  *********************
         } else if (command === 'env_file') {
           if (typeof service[command] === 'string') {
             serviceOverview[command] = service[command];
@@ -95,13 +93,106 @@ const InfoDropdown: React.FC<ReactProps> = ({ service, selectedContainer }) => {
             }
           }
         }
-        // if the service property is build and it's value is an object,
-        else if (command === 'build' && typeof service[command] === 'object') {
-          // set the command in the serviceOverview object to 'build' and set the value to an empty object
+        //  *********************
+        //  * Build
+        //  *********************
+        else if (command === 'build' && typeof service[command] !== 'string') {
         } else serviceOverview[command] = service[command];
       }
     });
   }
+
+  const infoToJsx = (
+    serviceOverview: ServiceOverview,
+    dockerComposeCommands: DockerComposeCommands,
+    environmentVariables: TwoDimension,
+    env_file: string[],
+    service: any,
+  ) => {
+    return Object.keys(serviceOverview).length === 0
+      ? 'There are no overview details in your Docker-Compose file'
+      : Object.keys(serviceOverview).map((command, i) => {
+          let commandJSX = (
+            <span className="command">{dockerComposeCommands[command]}</span>
+          );
+          let valueJSX: JSX.Element = <div></div>;
+          //  *********************
+          //  * Environment
+          //  *********************
+          if (command === 'environment' && !serviceOverview[command].length) {
+            const environment: JSX.Element[] = [];
+            Object.keys(environmentVariables).forEach(key => {
+              environment.push(
+                <li className="second-level" key={key}>
+                  <span>{key}:</span> {environmentVariables[key]}
+                </li>,
+              );
+            });
+            valueJSX = (
+              <span className="command-values">
+                <ul>{environment}</ul>
+              </span>
+            );
+            //  *********************
+            //  Env_File
+            //  *********************
+          } else if (command === 'env_file' && env_file.length) {
+            let envFileArray: JSX.Element[] = [];
+            env_file.forEach(el => {
+              envFileArray.push(
+                <li className="second-level" key={el}>
+                  {el}
+                </li>,
+              );
+            });
+            valueJSX = (
+              <span className="command-values">
+                <ul>{envFileArray}</ul>
+              </span>
+            );
+          } else if (command === 'build' && !serviceOverview[command].length) {
+            const build = service[command];
+            Object.keys(build).forEach(option => {
+              if (typeof build[option] === 'string') {
+                valueJSX = <span className="options">{build[option]}</span>;
+              } else if (Array.isArray(build[option])) {
+                const optionJSX = (build[option] as []).map(
+                  (element: string, i) => {
+                    return <li key={i}>{element}</li>;
+                  },
+                );
+                valueJSX = (
+                  <span className="third-level">
+                    <ul>{optionJSX}</ul>
+                  </span>
+                );
+              } else {
+                const optionJSX = Object.keys(build[option]).map(
+                  (key: string, i) => {
+                    return <li key={i}>{`${key}: ${build[option][key]}`}</li>;
+                  },
+                );
+                valueJSX = (
+                  <span className="third-level">
+                    <ul>{optionJSX}</ul>
+                  </span>
+                );
+              }
+            });
+          } else {
+            valueJSX = (
+              <span className="command-values">{serviceOverview[command]}</span>
+            );
+          }
+
+          return (
+            <div key={`command${i}`}>
+              {commandJSX}
+              {valueJSX}
+            </div>
+          );
+        });
+  };
 
   return (
     <div className="info-dropdown">
@@ -118,61 +209,13 @@ const InfoDropdown: React.FC<ReactProps> = ({ service, selectedContainer }) => {
           </Accordion.Toggle>
           <Accordion.Collapse eventKey="0">
             <Card.Body>
-              {Object.keys(serviceOverview).length === 0
-                ? 'There are no overview details in your Docker-Compose file'
-                : Object.keys(serviceOverview).map((command, i) => {
-                    let commandJSX = (
-                      <span className="command">
-                        {dockerComposeCommands[command]}
-                      </span>
-                    );
-                    let valueJSX: JSX.Element;
-                    if (
-                      command === 'environment' &&
-                      !serviceOverview[command].length
-                    ) {
-                      const environment: JSX.Element[] = [];
-                      Object.keys(environmentVariables).forEach(key => {
-                        environment.push(
-                          <li className="environment-variables" key={key}>
-                            <span>{key}:</span> {environmentVariables[key]}
-                          </li>,
-                        );
-                      });
-                      valueJSX = (
-                        <span className="command-values">
-                          <ul>{environment}</ul>
-                        </span>
-                      );
-                    } else if (command === 'env_file' && env_file.length) {
-                      let envFileArray: JSX.Element[] = [];
-                      env_file.forEach(el => {
-                        envFileArray.push(
-                          <li className="env-file-values" key={el}>
-                            {el}
-                          </li>,
-                        );
-                      });
-                      valueJSX = (
-                        <span className="command-values">
-                          <ul>{envFileArray}</ul>
-                        </span>
-                      );
-                    } else {
-                      valueJSX = (
-                        <span className="command-values">
-                          {serviceOverview[command]}
-                        </span>
-                      );
-                    }
-
-                    return (
-                      <div key={`command${i}`}>
-                        {commandJSX}
-                        {valueJSX}
-                      </div>
-                    );
-                  })}
+              {infoToJsx(
+                serviceOverview,
+                dockerComposeCommands,
+                environmentVariables,
+                env_file,
+                service,
+              )}
             </Card.Body>
           </Accordion.Collapse>
         </Card>
