038ac84ea0495858c1bb3355029880e72f2e72f6
Joshua Nordstrom <jdnordy@gmail.com>
2020-03-24 13:59:19 -0700

    added sizing to image in title

--- src/renderer/components/Title.tsx
+++ src/renderer/components/Title.tsx
@@ -7,7 +7,7 @@ type Props = {
 
 const Title: React.FC<Props> = props => (
   <div className="title">
-    <img src={getStatic('shell.png')} />
+    <img className="logo" src={getStatic('shell.png')} />
     <h1>Nautilus</h1>
   </div>
 );
