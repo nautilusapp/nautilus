cc7796306234210aaee9c4be85d66c4471aa68af
Joshua Nordstrom <jdnordy@gmail.com>
2020-03-24 13:55:48 -0700

    removed bootstrap from title

--- src/renderer/components/Title.tsx
+++ src/renderer/components/Title.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import Image from 'react-bootstrap/Image';
 import { getStatic } from '../helpers/static';
 
 type Props = {
@@ -8,7 +7,7 @@ type Props = {
 
 const Title: React.FC<Props> = props => (
   <div className="title">
-    <Image src={getStatic('shell.png')} fluid />
+    <img src={getStatic('shell.png')} />
     <h1>Nautilus</h1>
   </div>
 );
