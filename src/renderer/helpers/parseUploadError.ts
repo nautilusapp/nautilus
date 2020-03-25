684ec4f53b33b7e74ffabd24fb2807cd928a9176
Michael Dinh <michaeldinh1994@gmail.com>
2020-03-24 21:06:48 -0700

    update parsing logic

--- src/renderer/helpers/parseUploadError.ts
+++ src/renderer/helpers/parseUploadError.ts
@@ -2,10 +2,20 @@ const parseUploadError = (errorText: Error) => {
   //split string into an array from line breaks
   const splitErrorMessage = errorText.message.split('\n');
   //find the index where there is no information so we only parts of the error message we want
-  const startIndex =
-    splitErrorMessage.findIndex((line: string) => {
-      return line.includes('is invalid because');
-    }) + 1;
+  let startIndex = splitErrorMessage.findIndex((line: string) => {
+    return line.includes('is invalid because');
+  });
+
+  if (startIndex === -1) {
+    startIndex = splitErrorMessage.findIndex((line: string) => {
+      return line.includes('Command failed');
+    });
+  }
+
+  console.log(splitErrorMessage);
+  startIndex += 1;
+  console.log(startIndex);
+
   const paragraphIndex = splitErrorMessage.findIndex((line: string) => {
     return line === '';
   });
@@ -15,4 +25,5 @@ const parseUploadError = (errorText: Error) => {
   );
   return displayedErrorMessage;
 };
+
 export default parseUploadError;
