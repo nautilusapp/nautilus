651a0065792a3a132200347cf1ccd9271567950e
Sweet T <thurtt@rocketmail.com>
2020-03-25 17:59:56 -0700

    Fixed selectAll helper function to account for no longer having a depends option

--- src/renderer/helpers/selectAll.ts
+++ src/renderer/helpers/selectAll.ts
@@ -1,13 +1,13 @@
 import { State } from '../App.d';
 
 export const firstThree = (state: State): boolean => {
-  const optionValues = Object.values(state.options).slice(0, 3);
+  const optionValues = Object.values(state.options).slice(0, 2);
   let selected = 0;
   optionValues.forEach(val => {
     if (val === true) {
       selected += 1;
     }
   });
-  if (selected === 3) return true;
+  if (selected === 2) return true;
   return false;
 };
