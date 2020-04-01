513ed175a3445665b42833a3f6d3bbb2f0d914e9
Michael Dinh <michaeldinh1994@gmail.com>
2020-03-31 17:14:38 -0700

    jest setup

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -19,6 +19,7 @@ import { firstTwo } from './helpers/selectAll';
 import setGlobalVars from './helpers/setGlobalVars';
 import parseUploadError from './helpers/parseUploadError';
 import runDockerComposeValidation from '../common/dockerComposeValidation';
+import fs from 'fs';
 
 // IMPORT STYLES
 import './styles/app.scss';
@@ -136,6 +137,7 @@ class App extends Component<{}, State> {
   convertAndStoreYamlJSON = (yamlText: string) => {
     const yamlJSON = yaml.safeLoad(yamlText);
     const yamlState = convertYamlToState(yamlJSON);
+    fs.writeFileSync('./yamlstate.json', JSON.stringify(yamlState));
     setGlobalVars(yamlState.services);
     localStorage.setItem('state', JSON.stringify(yamlState));
     this.setState(Object.assign(initialState, yamlState));
