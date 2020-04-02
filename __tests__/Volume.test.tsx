fc4d52b5175a2fa834146be6bc0a91d8e6bdbfec
Aris Razuri <arazuli4@gmail.com>
2020-04-02 00:28:12 -0700

    add more test, Volume.test

--- __tests__/Volume.test.tsx
+++ __tests__/Volume.test.tsx
@@ -10,7 +10,8 @@ describe('<Volume/>', () => {
 
   const wrapper = shallow(<Volume {...props} />);
 
-  it('should render three divs', () => {
+  //test number of elements
+  it('should render four specific classNames', () => {
     expect(wrapper.find('.volumeLegend')).toHaveLength(1);
     expect(wrapper.find('.volumeColorName')).toHaveLength(1);
     expect(wrapper.find('.volumeSvgBox')).toHaveLength(1);
@@ -25,11 +26,22 @@ describe('<Volume/>', () => {
     expect(wrapper.find('rect')).toHaveLength(1);
   });
 
+  //test if rendering props
   it('make sure fill prop set to color, is passed into rect element', () => {
     expect(wrapper.find('.volumeSquare').props().fill).toEqual(props.color);
   });
 
   it('make sure p tag prop set to volume', () => {
     expect(wrapper.find('p').html()).toEqual(`<p>${props.volume}</p>`);
+  });
+
+  //test if parent nodes are correct
+  it('is svg element the parent of rect element', () => {
+    expect(
+      wrapper
+        .find('rect')
+        .parent()
+        .is('svg'),
+    ).toEqual(true);
   });
 });
