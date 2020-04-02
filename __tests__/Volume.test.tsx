1f86455953defbbbd21009e4c6cc30a43d4ca3b6
Aris Razuri <arazuli4@gmail.com>
2020-04-02 00:08:42 -0700

    adding tests for Volume

--- __tests__/Volume.test.tsx
+++ __tests__/Volume.test.tsx
@@ -1,17 +1,35 @@
 import React from 'react';
-import { shallow, configure } from 'enzyme';
+import { shallow } from 'enzyme';
 import Volume from '../src/renderer/components/Volume';
 
 describe('<Volume/>', () => {
   const props = {
-    volume: '',
-    color: '',
+    volume: 'volume',
+    color: 'blue',
   };
+
+  const wrapper = shallow(<Volume {...props} />);
+
   it('should render three divs', () => {
-    const wrapper = shallow(<Volume {...props} />);
     expect(wrapper.find('.volumeLegend')).toHaveLength(1);
     expect(wrapper.find('.volumeColorName')).toHaveLength(1);
     expect(wrapper.find('.volumeSvgBox')).toHaveLength(1);
     expect(wrapper.find('.volumeSquare')).toHaveLength(1);
+  });
+
+  it('should render one svg element', () => {
+    expect(wrapper.find('svg')).toHaveLength(1);
+  });
+
+  it('should render one rect element', () => {
+    expect(wrapper.find('rect')).toHaveLength(1);
+  });
+
+  it('make sure fill prop set to color, is passed into rect element', () => {
+    expect(wrapper.find('.volumeSquare').props().fill).toEqual(props.color);
+  });
+
+  it('make sure p tag prop set to volume', () => {
+    expect(wrapper.find('p').html()).toEqual(`<p>${props.volume}</p>`);
   });
 });
