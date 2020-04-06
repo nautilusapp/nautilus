0e35786da8526c7db3556f14388767ddd51e6f5f
Joshua Nordstrom <jdnordy@gmail.com>
2020-04-06 13:05:02 -0700

    added cache control

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,4 +8,4 @@ aws configure set aws_access_key_id $ACCESS_KEY_ID
 # set aws_secret_access_key
 aws configure set aws_secret_access_key $SECRET_ACCESS_KEY
 # sync release build to s3 buckets
-aws s3 sync ./release s3://$S3_BUCKET/release --exclude ".icon-set/*" --exclude "linux-unpacked/*" --exclude "mac/*" --exclude "win-unpacked/*"
+aws s3 sync ./release s3://$S3_BUCKET/release --exclude ".icon-set/*" --exclude "linux-unpacked/*" --exclude "mac/*" --exclude "win-unpacked/*" --cache-control max-age=10
